458ff83ca6657d06f67748bdc639cc04560a4a09
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-10-17 02:45:04 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 from setuptools import setup
 
 setup(
-    name='Jinxx66ObbTool',
-    version='1.0',
-    packages=['jinxx66obbitool'],
-    install_requires=[
-        # List your package dependencies here
-    ],
+    name="Jinxx66ObbTool",
+    version="1.0.0",
+    author="Your Name",
+    description="A simple Python package",
+    packages=["jinxx66obbitool"],
 )
