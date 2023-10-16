64fd6bfb3c38480593b58c1fcf15d54547a656ef
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-10-17 01:53:54 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Jinxx66ObbTool',
+    version='1.0',
+    packages=['jinxx66obbitool'],
+    install_requires=[
+        # List your package dependencies here
+    ],
+)
