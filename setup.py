a3498d9578797acafb4067cf7bded4a55ad7e9bc
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-11-20 02:01:39 +0530

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="Jinxx66ObbTool",
-    version="1.0.0",
-    author="Your Name",
-    description="A simple Python package",
-    packages=["jinxx66obbitool"],
-)
