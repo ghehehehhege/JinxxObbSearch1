79d04d7b9859c209a2c50d9c2253a1803a2241a9
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-11-29 03:38:42 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(
+    name='your-package-name',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        # your dependencies here
+    ],
+)
