04eea78a4954a1a3a4307605dd89b869d387d17a
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-11-29 04:22:21 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name='JinxxObbSearch',
-    version='0.1.3',
+    version='0.1.4',
     packages=find_packages(),
     install_requires=[
         'tqdm==4.62.2',
     ],
     entry_points={
         'console_scripts': [
-            'search = JinxxObbSearch.search:main',  # Adjust the entry point
+            'JinxxObbSearch = JinxxObbSearch.search:main',  # Adjust the entry point
         ],
     },
 )
