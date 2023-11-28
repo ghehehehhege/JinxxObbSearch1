29144df67f7e027a81bab7f4a657f0d37013673d
Jinxx6_6 <135565126+jinix6@users.noreply.github.com>
2023-11-29 03:59:10 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='your-package-name',
+    name='JinxxObbSearch',
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
